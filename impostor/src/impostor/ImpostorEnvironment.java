dda0b48b83b01d50dc6984909659e090407b21e5
Nahuel Nuñez <83452993+NahuelNu@users.noreply.github.com>
2024-04-29 00:40:07 -0300

    Arrancando Enviroment

--- impostor/src/impostor/ImpostorEnvironment.java
+++ impostor/src/impostor/ImpostorEnvironment.java
@@ -1,5 +1,25 @@
 package impostor;
 
-public class ImpostorEnvironment {
+import frsf.cidisi.faia.agent.Perception;
+import frsf.cidisi.faia.environment.Environment;
 
+public class ImpostorEnvironment  extends Environment{
+	
+	
+	public ImpostorEnvironment() {
+        // Create the environment state
+        this.environmentState = new ImpostorEnvironmentState();
+    }
+	
+	@Override
+    public ImpostorEnvironmentState getEnvironmentState() {
+        return (ImpostorEnvironmentState) super.getEnvironmentState();
+    }
+	
+	@Override
+	public Perception getPercept() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
--- impostor/src/impostor/ImpostorEnvironmentState.java
+++ impostor/src/impostor/ImpostorEnvironmentState.java
@@ -1,8 +1,18 @@
 package impostor;
 
+import java.util.List;
+import java.util.Map;
+
+import frsf.cidisi.faia.agent.Perception;
 import frsf.cidisi.faia.state.EnvironmentState;
 
 public class ImpostorEnvironmentState extends EnvironmentState {
+	
+	// HashMap1: key: AmbienteActual, value:
+	//										HashMap2: key: AmbientesAdyacentes, value: Percepciones 
+	private Map<RoomNave,Map<RoomNave,List<Perception>>> world; 
+	private Integer cantTripulantesVivos;
+	private List<RoomNave> tareasASabotear;
 	
 	// TODO
 	public ImpostorEnvironmentState() {
@@ -11,6 +21,8 @@ public class ImpostorEnvironmentState extends EnvironmentState {
 
 	@Override
 	public void initState() {
+		//Iniciar mapa
+		
 		// TODO Auto-generated method stub
 		
 	}
